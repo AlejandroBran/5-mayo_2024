93efa17da1229c55de03cfa4cf19ef658fdc7c55
AlejandroBran <00224824@uca.edu.sv>
2024-06-05 12:58:49 -0600

    5_de-Junio

---
+++ Sumatoria.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+for(int = 1; i<=t-w; i++){
+
+    suma=IBC +F/8.75 + suma,
+}
+
+float calcu_SBR(float IBC_C, float IBC)
+
+
+  if (opcion < 4){  
+        cout << "\nPension mensual. "<<"\n";
+        cout << "\n1. Pension para los optados."<<"\n";
+        cout << "\n2. Pension para los afiliados del Sistema Publico de Pensiones."<<"\n";
+        cout << "\n3. Pension de los afiliados al Sistema privado (AFP)."<<"\n";
+        cout << "\n4. Salir."<< "\n";
+        cout << "\nOpcion seleccionada.  "<<opcion<<"\n";
+        
